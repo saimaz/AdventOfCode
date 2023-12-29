960c70fc20d3a8d9de09da2875cdc5f3e288f154
Simonas Šerlinskas <saimaz@users.noreply.github.com>
2023-12-29 22:50:07 +0200

    input can be in new line, more clean way

--- 2023/d02/part1.py
+++ 2023/d02/part1.py
@@ -29,7 +29,7 @@ def calculate(data):
 
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(description="Calculate the sum for Advent of Code.")
+    parser = argparse.ArgumentParser()
     parser.add_argument("-t", "--test", action="store_true", help="Run with test data")
     parser.add_argument("-i", "--input", default="input.txt", help="Input file path")
     return parser.parse_args()
@@ -38,11 +38,13 @@ def parse_arguments():
 if __name__ == "__main__":
     args = parse_arguments()
 
-    test_data = """Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    test_data = """
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"""
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+"""
 
     if args.test:
         result = calculate(test_data)
